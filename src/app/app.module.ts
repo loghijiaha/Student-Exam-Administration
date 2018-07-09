c6943a5a17277380b0c999ce1a788660b7c44a74
rAWhiDe <ramitha.16@cse.mrt.ac.lk>
2018-07-09 13:25:51 +0530

    Validations preventing null objects in db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -131,6 +131,8 @@ const ROUTES: Route[] = [
     AddRepeatersComponent,
     ViewUserInquiriesComponent,
     ViewRepeatersComponent,
+    //AlertModule,
+    //CarouselModule,
 
   ],
   imports: [
